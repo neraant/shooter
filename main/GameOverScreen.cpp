dc7b2c1e7c28946db068b3974812e18c82a83ab2
Pavel <percheck298@gmail.com>
2023-12-28 17:09:38 +0300

    added gameover screen (.h and .cpp)

--- main/GameOverScreen.cpp
+++ main/GameOverScreen.cpp
@@ -1,13 +1,32 @@
 #include "GameOverScreen.h"
 
 GameOverScreen::GameOverScreen() {
-
+    if (!font.loadFromFile("fonts/ArialBold.ttf")) {
+        // Обработка ошибок при загрузке шрифта
+    }
+    message.setFont(font);
+    message.setCharacterSize(40); // в пикселях, а не в точках!
+    message.setFillColor(sf::Color::White);
+    message.setString("PRESS ENTER TO RESTART");
 }
 
 void GameOverScreen::draw(sf::RenderWindow& window) {
-   
+    // Получаем размеры окна
+    sf::Vector2u windowSize = window.getSize();
+
+    // Устанавливаем позицию текста в центре экрана
+    sf::FloatRect textRect = message.getLocalBounds();
+    message.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
+    message.setPosition(sf::Vector2f(windowSize.x / 2.0f, windowSize.y / 2.0f));
+
+    // Отрисовываем текст на экране
+    window.draw(message);
 }
 
 bool GameOverScreen::isRestart(sf::RenderWindow& window) {
-    
+    // Проверяем, нажата ли клавиша Enter
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter)) {
+        return true;
+    }
+    return false;
 }
